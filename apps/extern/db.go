93e80d4184a7ad555b2aebc54efc744553bb03cd
foxclore <32997971+foxclore@users.noreply.github.com>
2021-10-27 23:37:48 +0300

    Comments on db.go

--- apps/extern/db.go
+++ apps/extern/db.go
@@ -4,6 +4,7 @@ import "github.com/eXtern-OS/common/db"
 
 var dbc *db.DBClient
 
+// -- Default DB implementation, nothing to see here --
 const (
 	DatabaseName   = "Apps"
 	CollectionName = "Extern"
