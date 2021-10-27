896f0c82256895f5d0a31ca44a11a67696aac3d2
foxclore <32997971+foxclore@users.noreply.github.com>
2021-10-27 23:37:03 +0300

    Added comments in query.go

--- query/query.go
+++ query/query.go
@@ -1,11 +1,15 @@
 package query
 
+
+// Params represents parameter selection for the query
 type Params struct {
 	EnableFree         bool `json:"enable_free"`
 	EnablePaid         bool `json:"enable_paid"`
 	EnableSubscription bool `json:"enable_subscription"`
 }
 
+
+// Query provides structure for client-side queries
 type Query struct {
 	Query          string `json:"query"`
 	SnapEnabled    bool   `json:"snap_enabled"`
